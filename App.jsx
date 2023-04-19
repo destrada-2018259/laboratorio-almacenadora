359ded0fe7a21306dc497a9bbd3afe039517a42f
Jefferson Yuman Pérez <106627899+jyuman-2021335@users.noreply.github.com>
2023-04-18 19:02:58 -0600

    Versión Video To Do List
    Versión 1

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import { Header } from "./src/components/Header"
+import { Form   } from "./src/components/Form"
+import { TodosList } from "./src/components/TodosList"
+
+export const App = () => {
+
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];  
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);    
+  
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  return (
+    <div className="container">
+        <div className="app-wrapper">
+            <div>
+                <Header />
+            </div>
+            <div>
+                <Form 
+                    input={input}
+                    setInput={setInput}
+                    todos={todos}
+                    setTodos={setTodos}
+                    editTodo={editTodo}
+                    setEditTodo={setEditTodo}
+                />
+            </div>
+            <div>
+                <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+            </div>
+        </div>
+    </div> 
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+export const Header = () => {
+  return (
+    <>
+        <div className="header">
+            <h1>Todo-List</h1>
+        </div>
+    </>
+  )
+}
