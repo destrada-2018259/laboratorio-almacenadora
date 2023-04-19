248f07f2df9e9d94da018dfd2b2e322d3ad9a6be
Dorbal <daldana-2021604@kinal.edu.gt>
2023-04-19 00:23:23 -0600

    vista de la lista completa

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,45 +1,57 @@
 
 
-export const TodosList = ({todos, setTodos, setEditTodo}) => {
+export const TodosList = ({ todos, setTodos, setEditTodo }) => {
 
   const handleComplete = (todo) => {
     setTodos(
-        todos.map((item) => {
-            if(item.id === todo.id) {
-                return {...item, completed: !item.completed}
-            }
-            return item;
-        })
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed }
+        }
+        return item;
+      })
     )
-  }  
-  
-  const handleEdit = ({id}) => {
+  }
+
+  const handleEdit = ({ id }) => {
     const findTodo = todos.find((todo) => todo.id === id);
     setEditTodo(findTodo);
   }
 
-  const handleDelete = ({id}) => {
+  const handleDelete = ({ id }) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
 
   return (
     <div>
-        {todos.map((todo) => (
+      <table className="table">
+        <tbody>
+              
+          {todos.map((todo) => (
             <li className="list-item" key={todo.id}>
-                <input type="text" value={todo.title} className={`list ${todo.completed ? "complete" : ""}`} onChange={(event) => event.preventDefault()} />
-                <div>
-                    <button className="button-complete task-button" onClick={() => handleComplete(todo)}>
-                        <i className="fa fa-check-circle"></i>
-                    </button>
-                    <button className="button-edit task-button" onClick={() => handleEdit(todo)}>
-                        <i className="fa fa-edit"></i>
-                    </button>
-                    <button className="button-delete task-button" onClick={() => handleDelete(todo)}>
-                        <i className="fa fa-trash"></i>
-                    </button>
-                </div>
+              <tr >
+                <td scope="row" className={`list ${todo.completed ? "complete" : ""}`}>{todo.title}</td>
+                <td scope="row" className={`list ${todo.completed ? "complete" : ""}`}>Otto</td>
+                <td scope="row" className={`list ${todo.completed ? "complete" : ""}`}>@mdo</td>
+                <td scope="row" className={`list ${todo.completed ? "complete" : ""}`}>ddsfaf</td>
+                <td className={`list ${todo.completed ? "complete" : ""}`}>dfasfd</td>
+              </tr>
+              <div>
+                <button className="button-complete task-button" onClick={() => handleComplete(todo)}>
+                  <i className="fa fa-check-circle"></i>
+                </button>
+                <button className="button-edit task-button" onClick={() => handleEdit(todo)}>
+                  <i className="fa fa-edit"></i>
+                </button>
+                <button className="button-delete task-button" onClick={() => handleDelete(todo)}>
+                  <i className="fa fa-trash"></i>
+                </button>
+              </div>
             </li>
-        ))}
+          ))}
+
+        </tbody>
+      </table>
     </div>
   )
 }
